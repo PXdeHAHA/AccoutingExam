f00d1bb96cd8c68bbf76c11080131c2f68bc0ea8
Jaykong <kongyunpeng2011@sina.com>
2015-11-23 10:37:28 +0800

    Initial Commit

---
+++ AccountingExam/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AccountingExam
+//
+//  Created by trainer on 11/23/15.
+//  Copyright © 2015 trainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AccountingExam/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AccountingExam
+//
+//  Created by trainer on 11/23/15.
+//  Copyright © 2015 trainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
