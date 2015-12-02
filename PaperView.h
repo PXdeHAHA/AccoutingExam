e6c3fd8b143ca2b8331f8131b3002e1f8cf6ad4d
Jaykong <kongyunpeng2011@sina.com>
2015-12-02 08:34:10 +0800

    update images

---
+++ PaperView.h
@@ -0,0 +1,17 @@
+//
+//  PaperView.h
+//  AccountingExam
+//
+//  Created by kongyunpeng on 12/1/15.
+//  Copyright © 2015 trainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PaperView : UIView
+@property (weak, nonatomic) IBOutlet UISegmentedControl *segmentControl;
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+
+
+-(instancetype)initPaperView;
+@end
---
+++ PaperView.m
@@ -0,0 +1,18 @@
+//
+//  PaperView.m
+//  AccountingExam
+//
+//  Created by kongyunpeng on 12/1/15.
+//  Copyright © 2015 trainer. All rights reserved.
+//
+
+#import "PaperView.h"
+
+@implementation PaperView
+-(instancetype)initPaperView {
+    self = [super init];
+    if (self) {
+       _segmentControl addConstraint:<#(nonnull NSLayoutConstraint *)#>
+    }
+}
+@end
